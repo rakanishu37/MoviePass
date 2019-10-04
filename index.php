ac60cc99ae4c57ba6edfd44edd97d26a10734ce3
Federico Anastasi <47048387+rakanishu37@users.noreply.github.com>
2019-10-04 12:29:08 -0300

    Trae los generos

--- index.php
+++ index.php
@@ -5,6 +5,32 @@
 	Aca va a estar el new request, route
 	*/
 	//asdasdasd
+<?php
 
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "http://api.themoviedb.org/3/genre/movie/list?language=en-US&api_key=783ce81a4a4455d3719eb5ca1f039861",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_POSTFIELDS => "{}",
+));
+
+$response = curl_exec($curl);
+
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+var_dump ($response);
    
 ?>
