0845393d23de732e918b1b03cb7eeda2c834e191
Federico Anastasi <47048387+rakanishu37@users.noreply.github.com>
2019-10-04 11:48:45 -0300

    Revert "Revert "testeo""
    This reverts commit 1052227f6a012c5c77f81e27a6afce70e81ac1c9.

--- index.php
+++ index.php
@@ -1,21 +1,10 @@
 <?php
     include "autoload.php";
 
-    use models\carpeta1\Genero;
-
-
-    $var;
-    $varVacia = array();
-    $num = 5;
-    
-   $genero = array( new Genero('Horror'),
-                    new Genero('Accion'),
-                    new Genero('Comedia')
-                );
- 
-   
-    
-    var_dump(!isset($noExiste) && empty($noExiste));
+    /*
+	Aca va a estar el new request, route
+	*/
+	//asdasdasd
 
    
 ?>
