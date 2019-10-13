706fd6007035749774a01ed7d5c7efac362ae168
LautaroBG <48892699+LautaroBG@users.noreply.github.com>
2019-10-13 19:55:58 -0300

    The good ol' switcheroo

--- dao/DAOUser.php
+++ dao/DAOUser.php
@@ -1,12 +1,78 @@
 <?php
-    namespace dao;
-    use models\User as User;
+namespace dao;
+
+use dao\IDAOUser as IDAOUser;
+use models\User as User;
+
+class DAOUser implements IDAOUser
+{
+    private $userList;
     
-    interface IDAOUser {
-
-        function add(User $value);
-        function getAll();
-        //function delete(User $value);
-
+    public function add($newUser)
+    {
+        $this->retrieveData();
+        array_push($this->userList, $newUser);
+        $this->saveData();
     }
-?>
+
+    public function getAll()
+    {
+        $this->retrieveData();
+        return $this->userList;
+    }
+
+    public function getByEmail($email)
+    {
+        $this->retrieveData();
+        foreach ($this->userList as $User) {
+            if ($User->getEmail() == $email) {
+                return $User;
+            }
+        }
+    }
+
+    private function saveData()
+    {
+        $arrayToEncode = array();
+
+        foreach ($this->userList as $User) {
+            $valueArray['email'] = $User->getEmail();
+            $valueArray['password'] = $User->getPassword();
+            $valueArray['role'] = $User->getRole();
+            $valueArray['ticketPrice'] = $User->getTicketPrice();
+
+            array_push($arrayToEncode, $valueArray);
+        }
+        $jsonPath = $this->getJsonFilePath();
+        $jsonContent = json_encode($arrayToEncode, JSON_PRETTY_PRINT);
+        file_put_contents($jsonPath , $jsonContent);
+    }
+
+    private function retrieveData()
+    {
+        $this->userList = array();
+
+        $jsonPath = $this->getJsonFilePath();
+
+        // $jsonContent = file_get_contents('../Data/User.json');
+        $jsonContent = file_get_contents($jsonPath);
+
+        $arrayToDecode = ($jsonContent) ? json_decode($jsonContent, true) : array();
+
+        foreach ($arrayToDecode as $valueArray) {
+            $User = new User($valueArray['email'],$valueArray['password'],
+                    $valueArray['role']);
+            $User->setId($valueArray['id']);
+            
+            array_push($this->userList, $User);
+        }
+    }
+
+    function getJsonFilePath()
+    {
+        $jsonFilePath = ROOT."data/User.json";
+        if (!file_exists($jsonFilePath))
+            file_put_contents($jsonFilePath,"");
+        return $jsonFilePath;
+    }
+}
--- dao/IDAOUser.php
+++ dao/IDAOUser.php
@@ -1,78 +1,12 @@
 <?php
-namespace dao;
-
-use dao\IDAO as IDAO;
-use models\User as User;
-
-class DAOUser implements IDAOUser
-{
-    private $userList;
+    namespace dao;
+    use models\User as User;
     
-    public function add($newUser)
-    {
-        $this->retrieveData();
-        array_push($this->userList, $newUser);
-        $this->saveData();
+    interface IDAOUser {
+
+        function add(User $value);
+        function getAll();
+        //function delete(User $value);
+
     }
-
-    public function getAll()
-    {
-        $this->retrieveData();
-        return $this->userList;
-    }
-
-    public function getByEmail($email)
-    {
-        $this->retrieveData();
-        foreach ($this->userList as $User) {
-            if ($User->getEmail() == $email) {
-                return $User;
-            }
-        }
-    }
-
-    private function saveData()
-    {
-        $arrayToEncode = array();
-
-        foreach ($this->userList as $User) {
-            $valueArray['email'] = $User->getEmail();
-            $valueArray['password'] = $User->getPassword();
-            $valueArray['role'] = $User->getRole();
-            $valueArray['ticketPrice'] = $User->getTicketPrice();
-
-            array_push($arrayToEncode, $valueArray);
-        }
-        $jsonPath = $this->getJsonFilePath();
-        $jsonContent = json_encode($arrayToEncode, JSON_PRETTY_PRINT);
-        file_put_contents($jsonPath , $jsonContent);
-    }
-
-    private function retrieveData()
-    {
-        $this->userList = array();
-
-        $jsonPath = $this->getJsonFilePath();
-
-        // $jsonContent = file_get_contents('../Data/User.json');
-        $jsonContent = file_get_contents($jsonPath);
-
-        $arrayToDecode = ($jsonContent) ? json_decode($jsonContent, true) : array();
-
-        foreach ($arrayToDecode as $valueArray) {
-            $User = new User($valueArray['email'],$valueArray['password'],
-                    $valueArray['role']);
-            $User->setId($valueArray['id']);
-            
-            array_push($this->userList, $User);
-        }
-    }
-
-    function getJsonFilePath()
-    {
-        $jsonFilePath = ROOT."data/User.json";
-        if (!file_exists($jsonFilePath))
-            file_put_contents($jsonFilePath,"");
-        return $jsonFilePath;
-    }
-}
+?>
