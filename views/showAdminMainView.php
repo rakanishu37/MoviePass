114f75a8bfaf45eb449738b6cdffedd6b422baef
LautaroBG <48892699+LautaroBG@users.noreply.github.com>
2019-11-04 17:24:22 -0300

    Cambios Lucre

---
+++ views/showAdminMainView.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" media="screen" href="css/showAdminMainStyle.css">
+    <link rel="stylesheet" media="screen" href="css/buttonShowAdminMainStyle.css">
+    <!--<link rel="stylesheet" media="screen" href="<?php/* echo CSS_PATH */?>/header.css"> -->
+
+    <title>Movie Pass</title> 
+
+</head>
+<body>
+    
+    <!-- <?php/* require 'headerAdmi.php'; */?> -->
+        <table border="1">
+       
+                <tr>
+                    <th>Fecha</th>
+                    <th>Hora</th>
+                    <th>Lugar</th>
+                    <th>Pelicula</th>
+                    <th></th>
+                
+                </tr>
+        
+
+            
+                <tr>
+                    <td>11-11-19</td>
+                    <td>15:30</td>
+                    <td>Aldrey</td>
+                    <td>Shrek</td>
+                    <td><a class="image" href=""><img src="img/tachoDeBasura"></a></td>
+
+            
+                </tr>
+
+                <tr>
+                    <td>25-11-19</td>
+                    <td>10:00</td>
+                    <td>Ambassador</td>
+                    <td>Madagascar</td>
+
+                    <td><a class="image" href=""><img src="img/tachoDeBasura"></a></td>
+
+                
+                </tr>
+
+                <tr>
+                    <td>06-12-19</td>
+                    <td>23:00</td>
+                    <td>sfgasf</td>
+                    <td>afsdd</td>
+
+                    <td><a class="image" href=""><img src="img/tachoDeBasura"></a></td>
+
+                    
+                </tr>
+     
+        </table>
+
+
+
+    <a href="" class="add">
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    Agregar Funcion</a>
+
+    
+
+    
+</body>
+</html>
